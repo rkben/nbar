508e3cc37e9fa0c4dd0615a66b49e758fda605e5
Richard Benson <richard@rkben.dev>
2021-12-26 15:24:20 -0500

    add delay for everything but time, also rewrite later cause bad

--- src/nbar.nim
+++ src/nbar.nim
@@ -1,35 +1,52 @@
 import std/[os, json, times, strutils, strformat, osproc, parseopt]
 import system/io
 
+const delay = 5
+var lastTime: float = 0.0
+var lastTemp: float = 0.0
+var lastSpeed: int = 0
 proc cpuSpeed(): int =
   var line: string = readFile("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq")
   result = toInt(parseInt(line.strip()) / 1000)
+  lastSpeed = result
 
 proc cpuTemp(): float =
   let o = execProcess("sensors -j")
   let j = parseJson(o)
   result = j["coretemp-isa-0000"]["Package id 0"]["temp1_input"].getFloat()
+  lastTemp = result
 
 proc runJson(): bool =
-    let speed = cpuSpeed()
-    var speedcolor = "#FFFFFF"
-    let temp = cpuTemp()
-    var tempcolor = "#FFFFFF"
-    let time = now().format("dd-MM-yyyy :: HH:mm:ss")
-    var timecolor = "#FFFFFF"
-    if temp >= 60:
+  # TODO clean up delay handling
+  var speed = lastSpeed
+  var temp = lastTemp
+  var time = now().format("dd-MM-yyyy :: HH:mm:ss")
+  if (epochTime() - lastTime) >= delay:
+    speed = cpuSpeed()
+    temp = cpuTemp()
+    lastTime = epochTime()
+  var speedcolor = "#FFFFFF"
+  var tempcolor = "#FFFFFF"
+  var timecolor = "#FFFFFF"
+  if temp >= 60:
       tempcolor = "#A73333"
-    if speed <= 2000:
+  if speed <= 2000:
       speedcolor = "#959595"
-    echo ","
-    echo %*[{"full_text": fmt"{speed}MHz", "color": speedcolor},
-            {"full_text": fmt"{temp}°C", "color": tempcolor},
-            {"full_text": time, "color": timecolor}]
+  echo ","
+  echo %*[{"full_text": fmt"{speed}MHz", "color": speedcolor},
+          {"full_text": fmt"{temp}°C", "color": tempcolor},
+          {"full_text": time, "color": timecolor}]
 
 proc runTty(): bool =
-  let speed = cpuSpeed()
-  let temp = cpuTemp()
-  let time = now().format("dd-MM-yyyy :: HH:mm:ss")
+  # TODO clean up delay handling
+  var speed = lastSpeed
+  var temp = lastTemp
+  var time = now().format("dd-MM-yyyy :: HH:mm:ss")
+  echo epochTime()
+  if (epochTime() - lastTime) >= delay:
+    speed = cpuSpeed()
+    temp = cpuTemp()
+    lastTime = epochTime()
   echo fmt"{speed}MHz | {temp}°C | {time}"
 
 proc runHelp(): bool =
